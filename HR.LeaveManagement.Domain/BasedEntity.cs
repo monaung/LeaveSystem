a536590614f114905a86d895cc0f71aecf609e91
Wynn @ Mon Aung <mon.aung@gmail.com>
2025-02-05 00:02:51 +1300

    initial

---
+++ HR.LeaveManagement.Domain/BasedEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.LeaveManagement.Domain
+{
+    public abstract class BasedEntity
+    {
+        public int Id { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Domain/LeaveAllocations.cs
@@ -0,0 +1,10 @@
+﻿namespace HR.LeaveManagement.Domain
+{
+    public class LeaveAllocations : BasedEntity
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType? LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
